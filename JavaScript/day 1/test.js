af093aa03fb034a04fe1196a6e793b6ce64448ed
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-08-07 21:56:04 +0530

    update js day 1

--- JavaScript/day 1/test.js
+++ JavaScript/day 1/test.js
@@ -1,20 +1,35 @@
-var len = 100;
+// var len = 100;
 
-var data = {
-  name: "sachin singh",
-  dob: "24-03-1996",
-  prof: "mentor & sde2",
-};
+// var data = {
+//   name: "sachin singh",
+//   dob: "24-03-1996",
+//   prof: "mentor & sde2",
+// };
 
-if (data && data.name) {
-  var requireName = data.name.split(" ");
-}
+// if (data && data.name) {
+//   var requireName = data.name.split(" ");
+// }
 
-var requireData = {
-  firstName: requireName[0],
-  lastName: requireName[1],
-  date_of_birth: data.dob,
-  proffession: data.prof,
-};
+// var requireData = {
+//   firstName: requireName[0],
+//   lastName: requireName[1],
+//   date_of_birth: data.dob,
+//   proffession: data.prof,
+// };
 
-console.log("requireData", requireData);
+// console.log("requireData", requireData);
+
+let data = 100;
+console.log("data", data);
+
+//cons is not predefined keyword
+let cons = 200;
+console.log("cons", cons);
+
+/*
+SyntaxError: Unexpected token 'const'
+
+let const = 200;
+console.log("const", const);
+
+*/
