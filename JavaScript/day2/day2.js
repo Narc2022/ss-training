8772fcb92ec4d17f9fec9d15c9a4f3a823250c09
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-08-08 21:16:58 +0530

    day2

--- JavaScript/day2/day2.js
+++ JavaScript/day2/day2.js
@@ -1,1 +1,20 @@
-console.log("day2");
+/****************** declare example ********************/
+// let data = 100;
+// data = 50;
+// console.log("data", data);
+
+/****************** hoisting example********************/
+// console.log("hoist before", hoist); //output=> undefined
+
+// if (hoist) {
+//   console.log("have value");
+// } else {
+//   // fetch data from api
+//   hoist = null;
+// }
+
+// if (!hoist) {
+//   var hoist = "access this variable before declaration";
+// }
+// console.log("hoist", hoist);
+// // console.log("hoist after:", hoist);
